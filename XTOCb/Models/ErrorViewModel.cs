72db76087e59bed3df620ed43c8f170acb5960db
vxlam <vxlam@STEKLA>
2023-01-16 19:46:15 +0100

    Add project files.

---
+++ XTOCb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XTOCb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
